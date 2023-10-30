d3f034b75a79ba548e3e39f6c252b61c2f9ab3cd
Mirko Basic <bejzik8@gmail.com>
2023-10-30 01:08:34 +0100

    Finished deploy FundMe script.

--- deploy/01-deploy-fund-me.ts
+++ deploy/01-deploy-fund-me.ts
@@ -2,15 +2,34 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { network } from 'hardhat'
 
+import { developmentChains, networkConfig } from '../helper-hardhat.config'
+
 const deployFunction: DeployFunction = async function ({
     getNamedAccounts,
     deployments
 }: HardhatRuntimeEnvironment) {
-    console.log('Deploying fund me...')
-
-    const { deploy, log } = deployments
+    const { deploy, log, get } = deployments
     const { deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
+    const chainId = network.config.chainId!
+
+    let ethUsdPriceFeedAddress
+
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await get('MockV3Aggregator')
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
+    }
+
+    await deploy('FundMe', {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true
+    })
+
+    log('---------------------------')
 }
 
 export default deployFunction
+
+deployFunction.tags = ['all', 'fundme']
