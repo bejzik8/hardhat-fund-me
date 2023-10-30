6ddf8a22ada541d65062d60122c10d50fa3669e0
Mirko Basic <bejzik8@gmail.com>
2023-10-30 04:02:50 +0100

    Added more withdraw tests.

--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -101,5 +101,58 @@ describe('FundMe', () => {
                 endingDeployerBalance + gasCost
             )
         })
+
+        it('allows us to withdraw with multiple funders', async () => {
+            const accounts = await ethers.getSigners()
+
+            for (let i = 1; i < 6; i++) {
+                const fundMeConnectedContract = await fundMe.connect(
+                    accounts[i]
+                )
+
+                await fundMeConnectedContract.fund({ value: sendValue })
+            }
+
+            const fundMeAddress = await fundMe.getAddress()
+
+            const startingFundMeBalance =
+                await ethers.provider.getBalance(fundMeAddress)
+
+            const startingDeployerBalance = await ethers.provider.getBalance(
+                deployer.address
+            )
+
+            const transactionResponse = await fundMe.withdraw()
+            const transactionReceipt = (await transactionResponse.wait(
+                1
+            )) as ContractTransactionReceipt
+
+            const { gasUsed, gasPrice } = transactionReceipt
+
+            const gasCost = gasUsed * gasPrice
+
+            const endingFundMeBalance =
+                await ethers.provider.getBalance(fundMeAddress)
+            const endingDeployerBalance = await ethers.provider.getBalance(
+                deployer.address
+            )
+
+            assert.equal(endingFundMeBalance.toString(), '0')
+            assert.equal(
+                startingFundMeBalance + startingDeployerBalance,
+                endingDeployerBalance + gasCost
+            )
+
+            await expect(fundMe.funders(0)).to.be.reverted
+
+            for (let i = 1; i < 6; i++) {
+                assert.equal(
+                    (
+                        await fundMe.addressToAmountFunded(accounts[i].address)
+                    ).toString(),
+                    '0'
+                )
+            }
+        })
     })
 })
