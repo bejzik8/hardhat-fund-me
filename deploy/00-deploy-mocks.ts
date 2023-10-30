1e01a85b0ff046db464d2a5b85b6486bfabe9e01
Mirko Basic <bejzik8@gmail.com>
2023-10-30 01:02:37 +0100

    Created deploy mocks script.

---
+++ deploy/00-deploy-mocks.ts
@@ -0,0 +1,35 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { network } from 'hardhat'
+
+import {
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER
+} from '../helper-hardhat.config'
+
+const deployFunction: DeployFunction = async function ({
+    getNamedAccounts,
+    deployments
+}: HardhatRuntimeEnvironment) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    if (developmentChains.includes(network.name)) {
+        log('Local network detected! Deploying mocks...')
+
+        await deploy('MockV3Aggregator', {
+            contract: 'MockV3Aggregator',
+            from: deployer,
+            args: [DECIMALS, INITIAL_ANSWER],
+            log: true
+        })
+
+        log('Mocks deployed!')
+        log('---------------------------')
+    }
+}
+
+export default deployFunction
+
+deployFunction.tags = ['all', 'mocks']
